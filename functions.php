fa058280fec81863590557c2ed9c6b6aaa064ad1
Jake <mubok0212@gmail.com>
2017-06-29 00:52:40 +0900

    Creating Theme #5 - making submenu

--- functions.php
+++ functions.php
@@ -9,3 +9,13 @@ register_nav_menus(array(
   'primary' => __( 'Primary Menu' ),
   'footer' => __( 'Footer Menu' )
 ));
+
+// 상위 페이지 아이디 가져오기
+function get_top_parent_id(){
+    global $post;
+    if( $post->post_parent ){
+        $ancestors = array_reverse( get_post_ancestors( $post->ID ) );
+        return $ancestors[0];
+    }
+    return $post->ID;
+}
--- page.php
+++ page.php
@@ -3,8 +3,21 @@
         if(have_posts()) :
             while(have_posts()) : the_post(); ?>
                 <article class="post page">
-                    <h2><?php the_title(); ?></h2>
-                    <?php the_content(); ?>
+                   <h2><?php echo get_the_title( get_top_parent_id() ); ?></h2>
+                   <div class="content-box">
+                        <nav class="site-nav sub-nav">
+                            <ul>
+                            <?php
+                                $args = array(
+                                    'child_of' => get_top_parent_id(),
+                                    'title_li' => '',
+                                );
+                             ?>
+                            <?php wp_list_pages( $args ); ?>
+                            </ul>
+                        </nav>
+                        <?php the_content(); ?>
+                    </div>
                 </article>
       <?php endwhile;
         else :
