cf96419b7e407af03803ad20a7da609bf010ab3f
Jake <mubok0212@gmail.com>
2017-09-26 17:50:45 +0900

    making profile page

--- page-templates/template-profile.php
+++ page-templates/template-profile.php
@@ -55,68 +55,68 @@ if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POS
  ?>
 <?php $test = 0; ?>
 <?php get_header(); ?>
+
+<div class="content-box">
+
+  <div class="title-box">
+    <h2><?php echo ucfirst( the_title() ); ?></h2>
+  </div>
+
     <?php
         if(have_posts()) :
             while(have_posts()) : the_post(); ?>
             <div id="post-<?php the_ID(); ?>">
-    <div class="entry-content entry">
-        <?php the_content(); ?>
-        <?php if ( !is_user_logged_in() ) : ?>
-                <p class="warning">
-                    <?php _e('You must be logged in to edit your profile.', 'profile'); ?>
-                </p><!-- .warning -->
-        <?php else : ?>
-            <?php $test = 0; ?>
-            <?php if ( count($error) > 0 ) echo '<p class="error">' . implode(", ", $error) . '</p>'; ?>
-            <form method="post" id="adduser" action="<?php the_permalink(); ?>">
-                <p class="form-username">
-                    <label for="first-name"><?php _e('First Name', 'profile'); ?></label>
-                    <input class="text-input" name="first-name" type="text" id="first-name" value="<?php the_author_meta( 'first_name', $current_user->ID ); ?>" />
-                </p><!-- .form-username -->
-                <p class="form-username">
-                    <label for="last-name"><?php _e('Last Name', 'profile'); ?></label>
-                    <input class="text-input" name="last-name" type="text" id="last-name" value="<?php the_author_meta( 'last_name', $current_user->ID ); ?>" />
-                </p><!-- .form-username -->
-                <p class="form-email">
-                    <label for="email"><?php _e('E-mail *', 'profile'); ?></label>
-                    <input class="text-input" name="email" type="text" id="email" value="<?php the_author_meta( 'user_email', $current_user->ID ); ?>" />
-                </p><!-- .form-email -->
-                <p class="form-url">
-                    <label for="url"><?php _e('Website', 'profile'); ?></label>
-                    <input class="text-input" name="url" type="text" id="url" value="<?php the_author_meta( 'user_url', $current_user->ID ); ?>" />
-                </p><!-- .form-url -->
-                <p class="form-password">
-                    <label for="pass1"><?php _e('Password *', 'profile'); ?> </label>
-                    <input class="text-input" name="pass1" type="password" id="pass1" />
-                </p><!-- .form-password -->
-                <p class="form-password">
-                    <label for="pass2"><?php _e('Repeat Password *', 'profile'); ?></label>
-                    <input class="text-input" name="pass2" type="password" id="pass2" />
-                </p><!-- .form-password -->
-                <p class="form-textarea">
-                    <label for="description"><?php _e('Biographical Information', 'profile') ?></label>
-                    <textarea name="description" id="description" rows="3" cols="50"><?php the_author_meta( 'description', $current_user->ID ); ?></textarea>
-                </p><!-- .form-textarea -->
+            <div class="entry-content entry">
+                <?php the_content(); ?>
+                <?php if ( !is_user_logged_in() ) : ?>
+                        <p class="warning">
+                            <?php _e('You must be logged in to edit your profile.', 'profile'); ?>
+                        </p><!-- .warning -->
+                <?php else : ?>
+                    <?php $test = 0; ?>
+                    <?php if ( count($error) > 0 ) echo '<p class="error">' . implode(", ", $error) . '</p>'; ?>
 
-                <?php
-                    //action hook for plugin and extra fields
-                    do_action('edit_user_profile',$current_user);
-                ?>
-                <p class="form-submit">
-                    <?php //echo $referer; ?>
-                    <input name="updateuser" type="submit" id="updateuser" class="submit button" value="<?php _e('Update', 'profile'); ?>" />
-                    <?php wp_nonce_field( 'update-user' ) ?>
-                    <input name="action" type="hidden" id="action" value="update-user" />
-                </p><!-- .form-submit -->
-              </form><!-- #adduser -->
-          <?php endif; ?>
-      </div><!-- .entry-content -->
-  </div><!-- .hentry .post -->
+                    <h4>You can change your email and password in this page.</h4>
 
-      <?php endwhile;
-        else :
-            echo '포스트가 존재하지 않습니다.';
-        endif;
+                    <form method="post" id="adduser" action="<?php the_permalink(); ?>">
+                        <table id="user-input-form">
+                          <tr class="form-email">
+                            <td><label for="email"><?php _e('E-mail *', 'profile'); ?></label></td>
+                            <td>  <input class="text-input" name="email" type="text" id="email" value="<?php the_author_meta( 'user_email', $current_user->ID ); ?>" /></td>
+                          </tr>
+                          <tr class="form-password">
+                            <td><label for="pass1"><?php _e('Password *', 'profile'); ?> </label></td>
+                            <td><input class="text-input" name="pass1" type="password" id="pass1" /></td>
+                          </tr>
+                          <tr class="form-password">
+                            <td><label for="pass2"><?php _e('Repeat Password *', 'profile'); ?></label></td>
+                            <td><input class="text-input" name="pass2" type="password" id="pass2" /></td>
+                          </tr>
 
-     ?>
+                        </table>
+
+                        <?php
+                            //action hook for plugin and extra fields
+                            do_action('edit_user_profile',$current_user);
+                        ?>
+                        <p class="form-submit">
+                            <?php //echo $referer; ?>
+                            <input name="updateuser" type="submit" id="updateuser" class="submit button" value="<?php _e('Update', 'profile'); ?>" />
+                            <?php wp_nonce_field( 'update-user' ) ?>
+                            <input name="action" type="hidden" id="action" value="update-user" />
+                        </p><!-- .form-submit -->
+                      </form><!-- #adduser -->
+                  <?php endif; ?>
+              </div><!-- .entry-content -->
+          </div><!-- .hentry .post -->
+
+              <?php endwhile;
+                else :
+                    echo 'There is no user.';
+                endif;
+
+             ?>
+
+</div>
+
  <?php get_footer(); ?>
